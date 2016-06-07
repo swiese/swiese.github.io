0baee1fa014cef5d62af583c78b16fad17d48ad9
Sean Wiese <swiese@fb.com>
2016-06-07 11:25:56 -0700

    update

--- scripts/HomePage.js
+++ scripts/HomePage.js
@@ -1,12 +1,23 @@
+import Button from 'react-bootstrap/lib/Button';
+
 var HomePage = React.createClass({
   render: function() {
     return (
       <div className="container">
         <h1>Test App</h1>
+
       </div>
     );
   }
 });
+
+var LoginButton = React.createClass({
+  render: function() {
+    <Button bsStyle="success" bsSize="small" onClick={someCallback}>
+      Something
+    </Button>
+  };
+})
 
 ReactDOM.render(
   <HomePage/>,
